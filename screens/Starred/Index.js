cd6e7aa03026a87697c429a06ee11413ee7f42b0
Akshat Atray <akshatatray31@gmail.com>
2022-01-14 20:28:59 +0530

    RnVectorIcons Installed & Basic Screens Added! ✨

---
+++ screens/Starred/Index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const Starred = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+export default Starred
+
+const styles = StyleSheet.create({})
